d3541bf4c8917a8e0868048e840a3e3fa1875251
GuusLammers <guuslammers16@gmail.com>
2022-04-18 20:21:31 -0700

    Added method to model that shuffles sorting array

---
+++ sorting_visualizer/sorting_model.py
@@ -0,0 +1,24 @@
+import random
+
+import sorting_element as se
+import color as c
+
+
+class SortingModel:
+
+    def __init__(self, list_size: int) -> None:
+        self.list_size = list_size
+        self.sorting_list: list[se.SortingElement] = list()
+
+        self.shuffle_sorting_list()
+
+    def shuffle_sorting_list(self) -> None:
+        """Creates a list of SortingElements that have a random value and the color Color.WHITE"""
+        self.sorting_list = []
+        for _ in range(self.list_size):
+            rand = random.randint(1, self.list_size)
+            self.sorting_list.append(se.SortingElement(rand, c.Color.WHITE))
+
+    def get_sorting_list(self) -> "list[se.SortingElement]":
+        return self.sorting_list
+
