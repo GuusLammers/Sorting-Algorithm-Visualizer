3de4ef6b928849b19dc50f3ec91a04d45235b436
GuusLammers <guuslammers16@gmail.com>
2022-04-20 15:16:44 -0700

    Refactored project

--- app/main_controller.py
+++
@@ -1,8 +0,0 @@
-from main_model import *
-from main_view import *
-
-class MainController:
-
-    def __init__(self, main_model: MainModel, main_view: MainView) -> None:
-        self.main_view = main_view
-        self.main_model = main_model
